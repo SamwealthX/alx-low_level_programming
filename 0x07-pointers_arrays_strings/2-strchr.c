fd2325a8afaf15717ac5b775f9d7de1c1065cbb7
SamwealthX <Samwealthex@gmail.com>
2022-10-31 10:10:30 -0700

    Evenmore pointer,arrays, and string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,21 @@
   */
 char *_strchr(char *s, char c)
 {
-int a = 0, b;
+	int a = 0, b;
 
-while (s[a])
-{
-a++;
-}
+	while (s[a])
+	{
+		a++;
+	}
 
-for (b = 0; b <= a; b++)
-{
-if (c == s[b])
-{
-s += b;
-return (s);
-}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
